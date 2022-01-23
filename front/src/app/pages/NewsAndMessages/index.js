a244df7a7324b64cd26e2b3ffcaf9145c323f07a
Igor Filchev <filchev.igor@gmail.com>
2022-01-23 04:38:26 +0200

    PU-18 News and amount of consumed changes

--- front/src/app/pages/NewsAndMessages/index.js
+++ front/src/app/pages/NewsAndMessages/index.js
@@ -3,6 +3,26 @@ import Navbar from '../../components/Navbar';
 const NewsAndMessages = () => {
   return (`
     ${Navbar()}
+    
+    <div class="row">
+            <div class="col s12 m6">
+              <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                  <span class="card-title">Water supply halt</span>
+                  <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+                </div>
+              </div>
+              
+              <div class="card blue-grey darken-1">
+                <div class="card-content white-text">
+                  <span class="card-title">Electricity supply halt</span>
+                  <p>I am a very simple card. I am good at containing small bits of information.
+                  I am convenient because I require little markup to use effectively.</p>
+                </div>
+              </div>
+            </div>
+        </div>
   `);
 };
 
