95e1dcb94326152bdebab076b262724a273be775
Igor Filchev <90631197+filchev-igor@users.noreply.github.com>
2022-01-29 03:04:25 +0200

    PU-38 Contacts page design and dark light modes (#34)
    Co-authored-by: TomPosk <tomusiuxxxx@gmail.com>

--- front/src/app/pages/AmountOfConsumed/index.js
+++ front/src/app/pages/AmountOfConsumed/index.js
@@ -1,17 +1,120 @@
 import Navbar from '../../components/Navbar';
+import useLightMode from '../../utils/useLightMode';
 
 const AmountOfConsumed = async () => {
+    const { hasDayMode } = await useLightMode();
+
     return (`
         ${await Navbar()}
-    
-        #Insert table here (For current month and previous ones)
-        
-        <div class="container-fluid">
+
+        <div class="container-fluid ${!hasDayMode ? 'bg-night-mode' : ''} min-vh-100">
           <div class="row">
-          
+            <table class="table table-hover ${!hasDayMode ? 'border border-dark' : ''}" style="${!hasDayMode ? 'background-color: #b1b1b1;' : ''}">
+                <tbody>
+                    <tr>
+                      <th>Sausis 2022</th>
+                      <th>Skirtumas</th>
+                      <th>Skaitliuko rodmenys</th>
+                      <th></th>
+                    </tr>
+  
+                    <tr>
+                      <th>Karstas vanduo</th>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        <input type="number" style="width: 100px;">
+                      </td>
+                      <td>
+                        <button type="button" class="btn btn-outline-success">Irasyti pakeitimus</button>
+                      </td>
+                    </tr>
+                    
+                    <tr>
+                      <th>Saltas vanduo</th>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        <input type="number" style="width: 100px;">
+                      </td>
+                      <td>
+                        <button type="button" class="btn btn-outline-success">Irasyti pakeitimus</button>
+                      </td>
+                    </tr>
+    
+                    <tr>
+                      <th>Gruodis 2021</th>
+                      <th>Skirtumas</th>
+                      <th>Skaitliuko rodmenys</th>
+                      <th></th>
+                    </tr>
+                 
+                    <tr>
+                      <th>Karstas vanduo</th>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        
+                      </td>
+                    </tr>
+                    
+                    <tr>
+                      <th>Saltas vanduo</th>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        
+                      </td>
+                    </tr>
+    
+                    <tr>
+                      <th>Lapkritis 2021</th>
+                      <th>Skirtumas</th>
+                      <th>Skaitliuko rodmenys</th>
+                      <th></th>
+                    </tr>
+                
+                    <tr>
+                      <th>Karstas vanduo</th>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        
+                      </td>
+                    </tr>
+                    
+                    <tr>
+                      <th>Saltas vanduo</th>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        <span>6</span>
+                      </td>
+                      <td>
+                        
+                      </td>
+                    </tr>
+                </tbody>
+            </table>            
           </div>
         </div>
     `);
+
 };
 
 export default AmountOfConsumed;
